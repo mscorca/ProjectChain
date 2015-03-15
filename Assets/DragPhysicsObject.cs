06ca0ee2c3b320205376c0785c2bcfb7c1af7194
Matt Scorca <mscorca@ucsc.edu>
2015-03-14 21:10:21 -0700

    Draggable character, improved the physics of it all. Character Controller pretty broken, maybe leave that design behind all together.

---
+++ Assets/DragPhysicsObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class DragPhysicsObject : MonoBehaviour {
+     
+	public SpringJoint2D spring;
+ 
+      
+     void Awake(){
+		// "spring" is the SpringJoint2D component that I added to my object
+     	// i want the anchor position to start at the object's mousePosition
+	    spring = this.gameObject.GetComponent<SpringJoint2D>(); 
+	    spring.connectedAnchor = gameObject.transform.position;
+     }
+ 
+ 
+     void OnMouseDown(){
+	     //I'm reactivating the SpringJoint2D component each time I'm clicking on my object becouse I'm disabling it after I'm releasing the mouse click so it will fly in the direction i was moving my mouse
+	     spring.enabled = true;
+	 
+     }
+ 
+ 
+     void OnMouseDrag(){
+		if (spring.enabled = true){
+			//getting cursor position 
+			//the anchor get's cursor's position
+			Vector2 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			spring.connectedAnchor = cursorPosition;
+		}
+     }
+ 
+     
+    void OnMouseUp(){
+    	//disabling the spring component
+    	spring.enabled = false;
+    }
+ 
+ }
+ 
--- Assets/Scripts/CharacterControlTopDown.cs
+++ Assets/Scripts/CharacterControlTopDown.cs
@@ -4,45 +4,90 @@ using System.Collections;
 public class CharacterControlTopDown : MonoBehaviour {
 
 	private Rigidbody2D rigidbody2d;
+	//private Transform transform;
 	public float maxSpeed = 8.0f;
+	public float maxAngularSpeed = 5.0f;
 	public float moveForce = 365.0f;
+	public float spinForce = 10.0f;
+	private float spinDirectionMultiplier = 1;
+
+
+	private Vector3 prevMousePos;
+	private Vector3 mousePos;
+	private Vector3 prevCrossProduct;
+	private Vector3 crossProduct;
+	private Quaternion previousRot;
+	private Quaternion currentRot;
+
+
 
 	// Use this for initialization
 	void Start () {
 		rigidbody2d = GetComponent<Rigidbody2D>();
+		mousePos = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		// transform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		move();
+
+		prevMousePos = mousePos;
+		Vector3 drawPrev = Camera.main.transform.position - mousePos;
+		mousePos = Camera.main.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		Debug.Log("Pre: " + prevMousePos);
+		Debug.Log("Cur: " + mousePos);
+
+
+		//previousRot = currentRot;
+		//currentRot = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
+
+		Debug.DrawLine(Camera.main.transform.position, drawPrev, Color.blue);
+		Debug.DrawLine(Camera.main.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.red);
+		
+		float angle = Vector3.Angle(prevMousePos, mousePos);
+		prevCrossProduct = crossProduct;
+		crossProduct = Vector3.Cross(prevMousePos, mousePos);
+		if(crossProduct.y < 0 && !(prevCrossProduct.y < 0)){
+			spinDirectionMultiplier = -spinDirectionMultiplier;
+			Debug.Log("Angle: " + -angle);
+		} else {
+			Debug.Log("Angle: " + angle);
+		}
+
+		rigidbody2d.AddTorque(angle * spinDirectionMultiplier * spinForce);
+
+	}
+
+	void move(){
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
 
-		Debug.Log("Horizontal: " + h);
-		Debug.Log("Vertical: " + v);
+		//Debug.Log("Horizontal: " + h);
+		//Debug.Log("Vertical: " + v);
 
 		// Horizontal
 		if((h * rigidbody2d.velocity.x) < maxSpeed){
-			Debug.Log("FORCE: " + Vector2.right * h * moveForce);
+			//Debug.Log("FORCE: " + Vector2.right * h * moveForce);
 			rigidbody2d.AddForce(Vector2.right * h * moveForce);
 		} 
 
 		if(Mathf.Abs(rigidbody2d.velocity.x) > maxSpeed){
-			Debug.Log("Max speed achieved");
+			//Debug.Log("Max speed achieved");
 			rigidbody2d.velocity = new Vector2(Mathf.Sign(rigidbody2d.velocity.x) * maxSpeed, rigidbody2d.velocity.y);
 		}
 
 		// Vertical
 		if(v * rigidbody2d.velocity.y < maxSpeed){
-			Debug.Log("FORCE: " + Vector2.right * h * moveForce);
+			//Debug.Log("FORCE: " + Vector2.right * h * moveForce);
 			rigidbody2d.AddForce(Vector2.up * v * moveForce);
 		} 
 
 		if(Mathf.Abs(rigidbody2d.velocity.y) > maxSpeed){
-			Debug.Log("Max speed achieved");
+			//Debug.Log("Max speed achieved");
 			rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, Mathf.Sign(rigidbody2d.velocity.y) * maxSpeed);
 		}
-
-	//Debug.Log("Velocity: " + rigidbody2d.velocity);
 
 	}
 }
